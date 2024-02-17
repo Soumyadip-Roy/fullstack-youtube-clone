d73cfc5abbaf1fa9b1cd8b13114e9c85db936460
Soumyadip Roy <soumyadiproy1611@gmail.com>
2024-02-17 18:40:12 +0530

    connect to mongo atlas

---
+++ src/index.js
@@ -0,0 +1,30 @@
+// import mongoose from "mongoose";
+// import { DB_NAME } from "./constants";
+
+import connectDB from "./db/db.js";
+import dotenv from "dotenv";
+
+// import express from "express";
+// const app = express();
+
+// Approach 01
+// (async () => {
+//     try {
+//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+//         app.on("error", (err) => {
+//             console.error(err);
+//             throw err;
+//         });
+
+//         app.listen(process.env.PORT, () => {
+//             console.log(`listining on port ${process.env.PORT}`);
+//         });
+//     } catch (error) {
+//         console.error(error);
+//         throw error;
+//     }
+// })();
+
+ dotenv.config({path:'./env'})
+
+ connectDB();
