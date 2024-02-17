9be1cf674aecb6459df4f18f268aa5f222a21acb
Soumyadip Roy <soumyadiproy1611@gmail.com>
2024-02-18 01:37:38 +0530

    utility functions created

--- src/index.js
+++ src/index.js
@@ -27,4 +27,13 @@ import dotenv from "dotenv";
 
  dotenv.config({path:'./env'})
 
- connectDB();
+ connectDB()
+ .then(()=>{
+    app.listen(process.env.PORT || 8000),()=>{
+        console.log(`server is running at port ${process.env.PORT}`)
+    }
+ })
+ .catch((err) => {
+    console.log("Mongodb connection failed", err)
+ })
+
