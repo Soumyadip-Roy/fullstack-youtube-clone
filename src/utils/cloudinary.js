9c329e83714182689e52c6172c0af9cd45640ee1
Soumyadip Roy <soumyadiproy1611@gmail.com>
2024-02-20 00:29:35 +0530

    api working connected to db and files are uploading

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,23 +1,26 @@
-import {v2 as cloudinary} from 'cloudinary'
-import fs from "fs"
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
 
-cloudinary.config({ 
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key:  process.env.CLOUDINARY_API_KEY,
-  api_secret:  process.env.CLOUDINARY_API_SECRET,
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-const uploadONCloudinary = async (localFilePath) => {
+const uploadOnCloudinary = async (localFilePath) => {
     try {
-        if(!localFilePath) return null
-        //upload the file 
-        const response = await cloudinary.uploader.upload(localFilePath,{
-            resource_type:"auto"
-        })
-        console.log("file is uploaded in cloudinary",response,response.url)
-        return response
+        if (!localFilePath) return null;
+        //upload the file
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto",
+        });
+        console.log("file is uploaded in cloudinary", response.url);
+        fs.unlinkSync(localFilePath); 
+        return response;
     } catch (error) {
-        fdatasync.unlinkSync(localFilePath) //remove the locally saved temporary file 
+        fs.unlinkSync(localFilePath); //remove the locally saved temporary file
         return null;
     }
-}
+};
+
+export { uploadOnCloudinary };
